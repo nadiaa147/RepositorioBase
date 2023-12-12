27c4df35c6c8da4868ffd9256a652a76bd71f347
nadiaa <nadiacarballofigueroa@gmail.com>
2023-12-12 10:28:16 +0100

    cambié un archivo

---
+++ python/programas/04_operadores_aritmeticos.py
@@ -0,0 +1,47 @@
+
+a = 4 #asignamos un valor a a
+b = 9 #asignamos un valor a b
+
+resultado = a + b #indico la suma
+
+print (resultado)
+
+
+c = 6 #asignamos un valor a c
+d = 2 #asignamos un valor a d
+
+resultado = c - d #indico la resta
+
+print (resultado)
+
+
+e = 5 #asignamos un valor a e
+f = 4 #asignamos un valor a f
+
+resultado = e * f #indico la multiplicación
+
+print (resultado)
+
+
+g = 8 #asignamos un valor a g
+h = 2 #asignamos un valor a h
+
+resultado = g / h #indico la división
+
+print (resultado)
+
+
+i = 9 #asignamos un valor a i
+j = 2 #asignamos un valor a j
+
+resultado = i // j #indico la división entera
+
+print (resultado)
+
+i = 9 #asignamos un valor a i
+j = 2 #asignamos un valor a j
+
+resultado = i % j #indico el resultado
+
+print (resultado)
+
