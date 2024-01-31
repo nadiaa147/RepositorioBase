b9e87ac54843de01feea2deba138dac828efabad
nadiaa <nadiacarballofigueroa@gmail.com>
2024-01-31 10:04:35 +0100

    añadiendo el programa de funciones

---
+++ python/programas/04_funciones.py
@@ -0,0 +1,41 @@
+def suma():
+    num1=5
+    num2=3
+    print (num1+num2)
+    
+suma()
+
+def resta():
+    num1=7
+    num2=5
+    print (num1-num2)
+    
+resta ()
+
+def multiplicación():
+    num1=5
+    num2=4
+    print (num1*num2)
+    
+multiplicación()
+
+def división():
+    num1=18
+    num2=6
+    print (num1/num2)
+    
+división()
+
+def exponente():
+    num1=3
+    num2=4
+    print (num1**num2)
+    
+exponente()
+
+def división_entera():
+    num1=15
+    num2=2
+    print (num1//num2)
+
+división_entera()
